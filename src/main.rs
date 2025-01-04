315095958b799d2142e648305a17fa8a303cc676
Jonathan Routley <Jonathan.wei.liang@gmail.com>
2025-01-04 14:22:30 +0000

    add gitignore and test db

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,34 @@
 use iocraft::prelude::*;
 
-fn main() {
-    element! {
-        View(
-            border_style: BorderStyle::Round,
-            border_color: Color::Blue,
-        ) {
-            Text(content: "Hello, Fraser!")
+// fn main() {
+//     element! {
+//         View(
+//             border_style: BorderStyle::Round,
+//             border_color: Color::Blue,
+//         ) {
+//             Text(content: "Hello, Fraser!")
+//         }
+//     }
+//     .print();
+// }
+use sqlx::{migrate::MigrateDatabase, Sqlite, SqlitePool};
+
+const DB_URL: &str = "sqlite://sqlite.db";
+
+#[tokio::main]
+async fn main() {
+    if !Sqlite::database_exists(DB_URL).await.unwrap_or(false) {
+        println!("Creating database {}", DB_URL);
+        match Sqlite::create_database(DB_URL).await {
+            Ok(_) => println!("Create db success"),
+            Err(error) => panic!("error: {}", error),
         }
+    } else {
+        println!("Database already exists");
     }
-    .print();
+
+    let db = SqlitePool::connect(DB_URL).await.unwrap();
+
+    let result = sqlx::query("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY NOT NULL, name VARCHAR(250) NOT NULL);").execute(&db).await.unwrap();
+    println!("Create user table result: {:?}", result);
 }
