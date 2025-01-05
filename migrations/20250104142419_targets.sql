18fd8642a23971eaef21ff71966af0bd80981e04
Jonathan Routley <Jonathan.wei.liang@gmail.com>
2025-01-05 10:29:42 +0000

    allow count and value targets

--- migrations/20250104142419_targets.sql
+++ migrations/20250104142419_targets.sql
@@ -1,10 +1,14 @@
 CREATE TABLE targets (
     id INTEGER PRIMARY KEY,
-    name TEXT not NULL,
+    name TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     target_date DATE,
     status TEXT CHECK (status IN ('active', 'completed', 'abandoned')) DEFAULT 'active',
-    start_value REAL DEFAULT 0.0,
+    target_type TEXT CHECK (target_type IN ('count', 'value')) NOT NULL,
+    start_value REAL,
     target_value REAL,
-    target_type TEXT check(target_type IN ('count', 'value')) DEFAULT 'count'
-)
+    count_goal INTEGER,
+    value_goal REAL,
+    CHECK ((target_type = 'count' AND count_goal IS NOT NULL AND value_goal IS NULL) OR 
+          (target_type = 'value' AND value_goal IS NOT NULL AND count_goal IS NULL))
+);
